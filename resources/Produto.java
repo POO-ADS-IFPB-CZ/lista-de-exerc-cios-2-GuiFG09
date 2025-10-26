76619d9b291ebf9322a7c1964e70835a377d3af9
Guilherme Florencio <guiflorencio.08@outlook.com>
2025-10-25 23:53:58 -0300

    Create Produto class for product

--- resources/Produto.java
+++ resources/Produto.java
@@ -1,12 +1,49 @@
-import resources.Produto;
+package resources;
 
-public class Main {
-    public static void main(String[] args) {
-        Produto p1 = new Produto(101, "Mouse Gamer", 150.0, 20);
-        p1.exibirInfo();
+public class Produto {
+    
+    private int codigo;
+    private String nome;
+    private double preco;
+    private int estoque;
 
-        p1.setPreco(-50); // Teste da validação
-        p1.setPreco(120.0);
-        p1.exibirInfo();
+    public Produto(int codigo, String nome, double preco, int estoque) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
     }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setPreco(double preco) {
+       if (preco >=0) {
+           this.preco = preco;
+       }
+       else {
+           System.out.println("Erro: o preço não pode ser negativo.");
+       }
+    }
+
+    public void exibirInfo() {
+        System.out.println("Código: " + codigo);
+        System.out.println("Nome: " + nome);
+        System.out.println("Preço: R$" + preco);
+        System.out.println("Estoque: " + estoque + " unidades");
+    }
+
 }
